6565cc1e771f86d04ab36a936841089325453700
Megan Mai <59344390+megan-mai@users.noreply.github.com>
2022-03-12 00:07:26 -0800

    Add files via upload

---
+++ web2.js
@@ -0,0 +1,40 @@
+//hidden bio
+var text = document.getElementById("hidden");
+var btnText = document.getElementById("more");
+
+text.style.marginLeft = null;
+text.style.opacity = 0;
+
+function toggleText() {
+  if (text.style.marginLeft) {
+    btnText.innerHTML = "More →";
+    text.style.marginLeft = null;
+    text.style.opacity = 0;
+
+  } else {
+    btnText.innerHTML = "Less ←";
+    text.style.marginLeft = 0 + "px";
+    text.style.opacity = 1;
+
+  }
+}
+
+//overlay
+function openNav(overlay) {
+  document.getElementById(overlay).style.marginLeft = 0 + "px";
+}
+
+function closeNav(overlay) {
+  document.getElementById(overlay).style.marginLeft = null;
+}
+
+function toggleImg(overlay, id) {
+var array = document.getElementsByClassName(overlay);
+for (i = 0; i < array.length; i++){
+  if(id === array[i].id){
+    array[i].style.display = "block";
+  }else {
+    array[i].style.display = "none";
+  }
+}
+}
